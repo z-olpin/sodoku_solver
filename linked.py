c6f4ad225fe18855d2cd97b0ffc05979c65f2c0a
Zachary Olpin <reedwinters@gmail.com>
2019-05-16 13:38:32 -0600

    pleasing flake8

--- linked.py
+++ linked.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 
-board_list = [] #(board, last_tried)
 testboard = [None, None, None, 6, 1, 9, 7, 5, 3,
              3, 9, 1, 7, 5, 2, 6, 8, 4,
              6, None, None, None, 8, 4, 1, 9, 2,
@@ -12,26 +11,35 @@ testboard = [None, None, None, 6, 1, 9, 7, 5, 3,
              2, 7, 5, 1, 9, 8, None, None, None,
              9, 1, 6, 2, 4, None, None, None, None]
 
-def get_square_neighbors(ind):
+board_list = []
 
+
+def print_board(board):
+    board = np.array(board).reshape((9, 9))
+    for row in board:
+        print(*row)
+
+
+def get_square_neighbors(ind):
     col_num = ind % 9
-    #(left=.0, center=.3, right-.6)
-    if 0.01 > ((float(col_num) / 3.0) - float(math.floor(col_num / 3.0))) > -0.01:
+    placer = (float(col_num) / 3.0) - float(math.floor(col_num / 3.0))
+    if 0.01 > (placer) > -0.01:
         lcr = 'l'
-    elif 0.34 > ((float(col_num) / 3.0) - float(math.floor(col_num / 3.0))) > 0.32:
+    elif 0.34 > (placer) > 0.32:
         lcr = 'c'
-    elif 0.67 > ((float(col_num) / 3.0) - float(math.floor(col_num / 3.0))) > 0.65:
+    elif 0.67 > (placer) > 0.65:
         lcr = 'r'
 
-    trans_ind_map = list(np.concatenate(np.transpose(np.arange(81).reshape((9, 9)))))
-    trans_ind = trans_ind_map.index(ind)
-    trans_col = trans_ind % 9
-    #(left=.0, center=.3, right-.6)
-    if 0.01 > ((float(trans_col) / 3.0) - float(math.floor(trans_col / 3.0))) > -0.01:
+    transposed_inds = list(np.concatenate(np.transpose(np.arange(81).reshape((9, 9)))))
+    t_ind = transposed_inds.index(ind)
+    t_col = t_ind % 9
+    t_placer = (float(t_col) / 3.0) - float(math.floor(t_col / 3.0))
+
+    if 0.01 > (t_placer) > -0.01:
         trans_lcr = 'l'
-    elif 0.34 > ((float(trans_col) / 3.0) - float(math.floor(trans_col / 3.0))) > 0.32:
+    elif 0.34 > (t_placer) > 0.32:
         trans_lcr = 'c'
-    elif 0.67 > ((float(trans_col) / 3.0) - float(math.floor(trans_col / 3.0))) > 0.65:
+    elif 0.67 > (t_placer) > 0.65:
         trans_lcr = 'r'
     lcr += trans_lcr
 
@@ -53,23 +61,27 @@ def get_square_neighbors(ind):
         target = ind + 8
     elif lcr == 'cc':
         target = ind
-    square_neighbors = [target-10, target-9, target-8, target-1, target+1, target+8, target+9, target+10]
-    return square_neighbors
+    return [target-10, target-9, target-8, target-1,
+            target+1, target+8, target+9, target+10]
+
 
 def get_col_neighbors(ind):
     col_num = ind % 9
-    col_neighbors = list(map(lambda x: x + col_num, [0, 9, 18, 27, 36, 45, 54, 63, 72]))
+    col_neighbors = list(map(lambda x: x + col_num,
+                             [0, 9, 18, 27, 36, 45, 54, 63, 72]))
     col_neighbors.remove(ind)
     return (col_num, col_neighbors)
+
 
 def get_row_neighbors(ind, col_num):
     row_neighbors = list(range(ind - col_num, ind - col_num + 9))
     row_neighbors.remove(ind)
     return row_neighbors
 
-def solve_board(board,last_tried=0, ind=None):
+
+def solve_board(board, last_tried=0, ind=None):
     if None not in board:
-        print(board)
+        print_board(board)
         return
     else:
         if not ind:
@@ -81,11 +93,10 @@ def solve_board(board,last_tried=0, ind=None):
         possible_nums.sort()
 
         if last_tried in possible_nums:
-            del possible_nums[:possible_nums.index(last_tried)]
-            possible_nums.remove(last_tried)
+            del possible_nums[:possible_nums.index(last_tried)+1]
 
         if len(possible_nums) == 0:
-            if board_list[-1][0] == False:
+            if board_list[-1][0] is False:
                 last_tried = board_list[-1][2]
                 ind = board_list[-1][3]
                 board = board_list[-1][1]
@@ -101,7 +112,7 @@ def solve_board(board,last_tried=0, ind=None):
             del board[ind]
             board.insert(ind, possible_nums[0])
             board_list.append((board, possible_nums[0], ind))
-        
         solve_board(board, possible_nums[0])
+
 
 solve_board(testboard)
