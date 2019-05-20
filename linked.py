0e984e85d43f3fb180c0aa82fc8d48c7e3ff8874
Zachary Olpin <reedwinters@gmail.com>
2019-05-19 21:11:40 -0600

    removed numpy dependency

--- linked.py
+++ linked.py
@@ -1,92 +1,36 @@
-import numpy as np
 import math
 
 testboard = [None, None, None, 6, 1, 9, 7, 5, 3,
-             3, 9, 1, 7, 5, 2, 6, 8, 4,
+             None, None, None, None, None, None, None, None, None,
              6, None, None, None, 8, 4, 1, 9, 2,
              1, 3, 9, 4, 2, 7, 8, 6, 5,
              None, None, None, 9, 6, 1, 2, 3, 7,
              7, 6, 2, 8, 3, 5, 4, 1, 9,
-             8, 4, 3, 5, 7, 6, 9, 2, 1,
+             8, None, None, None, 7, 6, 9, 2, 1,
              2, 7, 5, 1, 9, 8, None, None, None,
-             9, 1, 6, 2, 4, None, None, None, None]
+             None, None, None, None, None, None, None, None, None]
 
 board_list = []
 
-
-def print_board(board):
-    board = np.array(board).reshape((9, 9))
-    for row in board:
-        print(*row)
-
-
-def get_square_neighbors(ind):
-    col_num = ind % 9
-    placer = (float(col_num) / 3.0) - float(math.floor(col_num / 3.0))
-    if 0.01 > (placer) > -0.01:
-        lcr = 'l'
-    elif 0.34 > (placer) > 0.32:
-        lcr = 'c'
-    elif 0.67 > (placer) > 0.65:
-        lcr = 'r'
-
-    transposed_inds = list(np.concatenate(np.transpose(np.arange(81).reshape((9, 9)))))
-    t_ind = transposed_inds.index(ind)
-    t_col = t_ind % 9
-    t_placer = (float(t_col) / 3.0) - float(math.floor(t_col / 3.0))
-
-    if 0.01 > (t_placer) > -0.01:
-        trans_lcr = 'l'
-    elif 0.34 > (t_placer) > 0.32:
-        trans_lcr = 'c'
-    elif 0.67 > (t_placer) > 0.65:
-        trans_lcr = 'r'
-    lcr += trans_lcr
-
-    if lcr == 'll':
-        target = ind + 10
-    elif lcr == 'lc':
-        target = ind + 1
-    elif lcr == 'lr':
-        target = ind - 8
-    elif lcr == 'cl':
-        target = ind + 9
-    elif lcr == 'cr':
-        target = ind - 9
-    elif lcr == 'rr':
-        target = ind - 10
-    elif lcr == 'rc':
-        target = ind - 1
-    elif lcr == 'rl':
-        target = ind + 8
-    elif lcr == 'cc':
-        target = ind
-    return [target-10, target-9, target-8, target-1,
-            target+1, target+8, target+9, target+10]
-
-
-def get_col_neighbors(ind):
-    col_num = ind % 9
-    col_neighbors = list(map(lambda x: x + col_num,
-                             [0, 9, 18, 27, 36, 45, 54, 63, 72]))
-    col_neighbors.remove(ind)
-    return (col_num, col_neighbors)
-
-
-def get_row_neighbors(ind, col_num):
-    row_neighbors = list(range(ind - col_num, ind - col_num + 9))
-    row_neighbors.remove(ind)
-    return row_neighbors
+def get_neighbors(i):
+    row = i // 9
+    col = i % 9
+    square_i = ((row % 3) * 3) + (i % 3)
+    square_t_targets = {0: 10, 1: 9, 2: 8, 3: 1, 4: 0, 5: -1, 6: -8, 7: -9, 8: -10}
+    target_i = square_t_targets[square_i] + i
+    square_neighbors = [target_i + v for v in square_t_targets.values()]
+    col_neighbors = list(map(lambda x: x + col, [0, 9, 18, 27, 36, 45, 54, 63, 72]))
+    row_neighbors = list(range(i - col, i - col + 9))
+    all_neighbors = (row_neighbors + col_neighbors + square_neighbors)
+    return all_neighbors
 
 
 def solve_board(board):
     for ind, v in enumerate(board):
         if v != None:
             continue
-        col_num, col_neighbors = get_col_neighbors(ind)
-        row_neighbors = get_row_neighbors(ind, col_num)
-        square_neighbors = get_square_neighbors(ind)
-        possible_nums = list({1, 2, 3, 4, 5, 6, 7, 8, 9} - set([board[e] for e in (row_neighbors + col_neighbors + square_neighbors)]))
+        neighbors = get_neighbors(ind)
+        possible_nums = list({1, 2, 3, 4, 5, 6, 7, 8, 9} - set([board[e] for e in (neighbors)]))
 
         for n in possible_nums:
             try_board = board.copy()
@@ -99,4 +43,4 @@ def solve_board(board):
     return board
 
 
-print_board(solve_board(testboard))
+print(solve_board(testboard))
