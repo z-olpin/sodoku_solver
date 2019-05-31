d52b74acc8c48ecd35274ca1a4c0a4f2a30ac6f6
zach <38638365+vxxce@users.noreply.github.com>
2019-05-30 21:47:50 -0600

    Changed testboard to "world's hardest sodoku"
    "World's hardest" according to The Telegraph (https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html)

--- solver.py
+++ solver.py
@@ -1,14 +1,14 @@
 import math
 
-testboard = [None, None, None, 6, 1, 9, 7, 5, 3,
-             None, None, None, None, None, None, None, None, None,
-             6, None, None, None, 8, 4, 1, 9, 2,
-             1, 3, 9, 4, 2, 7, 8, 6, 5,
-             None, None, None, 9, 6, 1, 2, 3, 7,
-             7, 6, 2, 8, 3, 5, 4, 1, 9,
-             8, None, None, None, 7, 6, 9, 2, 1,
-             2, 7, 5, 1, 9, 8, None, None, None,
-             None, None, None, None, None, None, None, None, None]
+testboard = [8, None, None, None, None, None, None, None, None,
+             None, None, 3, 6, None, None, None, None, None,
+             None, 7, None, None, 9, None, 2, None, None,
+             None, 5, None, None, None, 7, None, None, None,
+             None, None, None, None, 4, 5, 7, None, None,
+             None, None, None, 1, None, None, None, 3, None,
+             None, None, 1, None, None, None, None, 6, 8,
+             None, None, 8, 5, None, None, None, 1, None,
+             None, 9, None, None, None, None, 4, None, None]
 
 def print_board(board):
     for i in range(9):
