042efa112b92642aff0c5c63a825118f0dce1a9b
Zachary Olpin <zach@zacharyolp.in>
2019-07-27 03:41:27 -0600

    better board design, not wired yet

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,43 @@ import React, { useState, useEffect } from 'react'
 import solveBoard from './solver'
 import styles from '../static/styles'
 
+
+let Board = () => {
+  return (
+    <div className="Board" style={{margin: '2rem', width: '456px', display: 'flex', flexDirection: 'column', flexBasis: '1', border: '2px solid black'}}>
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  )
+}
+
+let Row = () => {
+  return (
+    <div className="row" style={{flex: '1', display: 'flex'}}>
+      <Square /><Square /><Square />
+    </div>
+  )
+}
+
+let Square = () => {
+  return (
+    <div className="square" style={{flex: '1', border: '1px solid black', width: '150px'}}>
+      <InputRow />
+      <InputRow />
+      <InputRow />
+    </div>
+  )
+}
+
+let InputRow = () => {
+  return (
+    <div className="inputRow" style={{flexDirection: 'row', display: 'flex', width: '150px'}}>
+      {[...Array(3).keys()].map(n => <input type="text" maxLength={1} onChange={()=>{}} style={{width: '50px', height: '50px', border: '1px solid black'}}/>)}
+    </div>
+  )
+}
+
 let SolvedDisplay = (props) => {
   const [solvedBoard, setSolvedBoard] = useState([...Array(81)])
 
@@ -55,6 +92,7 @@ let InputBoard = () => {
   return (
     <div>
       <div>
+        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}}><Board /><Board /></div>
         <div>{[...Array(9).keys()].map(n => <input type="text" maxLength={1} onChange={entry} id={n+ 0} style={{...((n%3==0 && !(n%9==0)) ? styles.inputLeftBorder: styles.inputStyles)}} />)}</div>
         <div>{[...Array(9).keys()].map(n => <input type="text" maxLength={1} onChange={entry} id={n+ 9} style={{...((n%3==0 && !(n%9==0)) ? styles.inputLeftBorder : styles.inputStyles)}} />)}</div>
         <div>{[...Array(9).keys()].map(n => <input type="text" maxLength={1} onChange={entry} id={n+18} style={{...((n%3==0 && !(n%9==0)) ? styles.inputLeftBorder : styles.inputStyles), ...styles.inputBottomBorder}} />)}</div>
