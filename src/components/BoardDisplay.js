4445616b6830fdb51490b8e09c937537df44a514
Zachary Olpin <zach@zacharyolp.in>
2019-08-17 22:30:33 -0600

    Disallow invalid inputs (incl rendering, event styling, state updating)

--- src/components/BoardDisplay.js
+++ src/components/BoardDisplay.js
@@ -1,33 +1,43 @@
-import React, { useState } from 'react'
-import SolvedBoard from './SolvedBoard'
-import InputBoard from "./InputBoard"
-import solveBoard from './solver'
+import React, { useState } from "react";
+import SolvedBoard from "./SolvedBoard";
+import InputBoard from "./InputBoard";
+import { getNeighbors, solveBoard } from "./solver";
 
 const BoardDisplay = () => {
-
-  const [board, setBoard] = useState([...Array(81)])
-  const [solvedBoard, setSolvedBoard] = useState([...Array(81)])
+  const [board, setBoard] = useState([...Array(81)]);
+  const [solvedBoard, setSolvedBoard] = useState([...Array(81)]);
 
   const onInput = (id, value) => {
-    document.getElementById(id).style.backgroundColor = 'lightgrey'
-    document.getElementById(id+100).style.backgroundColor = 'lightgrey'
-    document.getElementById(id+100).innerText = value
-    const newBoard = [...board.slice(0, id), value, ...board.slice(id + 1)]
-    setBoard(newBoard)
-  }
+    let neighbors = new Set(getNeighbors(id));
+    neighbors = [...Array(...neighbors)];
+    const neighborVals = neighbors.map(i => board[i]);
 
-  const handleSolve = (e) => {
-    e.preventDefault()
-    const newBoard = solveBoard([...board])
-    setSolvedBoard([...newBoard])
-  }
+    if (neighborVals.includes(value)) {
+      document.getElementById(id).value = "";
+    }
+
+    if (!neighborVals.includes(value)) {
+      document.getElementById(id).style.backgroundColor = "lightgrey";
+      document.getElementById(id + 100).style.backgroundColor = "lightgrey";
+      document.getElementById(id + 100).innerText = value;
+      const newBoard = [...board.slice(0, id), value, ...board.slice(id + 1)];
+      setBoard(newBoard);
+      console.log(board);
+    }
+  };
+
+  const handleSolve = e => {
+    e.preventDefault();
+    const newBoard = solveBoard([...board]);
+    setSolvedBoard([...newBoard]);
+  };
 
   return (
     <>
       <InputBoard handleSolve={handleSolve} onInput={onInput} />
       <SolvedBoard solvedBoard={solvedBoard} oldBoard={board} />
     </>
-  )
-}
+  );
+};
 
-export default BoardDisplay
+export default BoardDisplay;
